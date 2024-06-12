e4185e692a82aab4cbd98f3841e61ad0488ef189
Hammas_Shahid <143096092+Hammas-Shahid@users.noreply.github.com>
2024-06-12 15:59:15 +0500

    Lead call Bug fix

--- src/leads/lead-calls/entities/lead-call.entity.ts
+++ src/leads/lead-calls/entities/lead-call.entity.ts
@@ -1,9 +1,10 @@
 import { Disposition } from 'src/dispositions/entities/disposition.entity';
 import { Lead } from 'src/leads/entities/lead.entity';
+import { BasicEntity } from 'src/shared/entities/base-entity';
 import { Column, Entity, ManyToOne } from 'typeorm';
 
 @Entity()
-export class LeadCall {
+export class LeadCall extends BasicEntity {
   @Column()
   duration: string;
 
